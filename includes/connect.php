29dc896982590ce2dbc0ef40883626b07208878f
Katie Andrews <kandrews@friendscentral.org>
2011-06-02 09:22:15 -0700

    Edited includes/connect.php via GitHub

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "Photo";
+	$server = "10.194.111.8";
+	$username = "user_cb1f658c";
+	$password = "EH;KlGeiZpW(q.";
+	$database = "db_cb1f658c";
 
 		mysql_connect($server, $username, $password) or die ("Could not connect to server");
 		mysql_select_db($database) or die ("Could not connect to database");
